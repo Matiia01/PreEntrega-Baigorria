7d26e74976fba459706d79fb1005f909ae8d64e8
Matiia01 <baigorria2019@hotmail.com>
2023-10-23 19:26:43 -0300

    Prueba PF

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import Cart from './components/Cart';
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product, quantity) => {
+    // Copia el carrito actual y agrega el nuevo producto con la cantidad
+    setCart([...cart, { ...product, quantity }]);
+  };
+
+  return (
+    <Router>
+      <div>
+        <NavBar cartCount={cart.length} />
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting="¡Bienvenido a FireShop!" addToCart={addToCart} />} />
+          <Route path="/category/:category" element={<ItemListContainer addToCart={addToCart} />} />
+          <Route path="/item/:id" element={<ItemDetailContainer addToCart={addToCart} />} />
+          <Route path="/cart" element={<Cart cart={cart} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
